a5c8e42005e65533c5146af391d33a9609893eb1
seeker-mm-1499 <myintsein1999.mm9@gmail.com>
2024-03-03 18:46:54 +0630

    using bootstrap with sprint starter

---
+++ using-bootstrap/src/main/java/com/jdc/demo/UsingBootstrapApplication.java
@@ -0,0 +1,13 @@
+package com.jdc.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsingBootstrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsingBootstrapApplication.class, args);
+	}
+
+}
